e24ff175bef8c4c2123ad170c0c971904d104ad7
jjunohj <121740394+jjunohj@users.noreply.github.com>
2024-03-14 20:17:44 +0900

    [#11] Init: Liveblocks 캔버스 Next 14 마이그레이션 (#4)
    * init/#3 : 첫 커밋
    
    * build/#3 : 의존성 추가 - nanoid, perfect-freehand
    
    * build: liveblocks - Presence, Storage 내 속성 추가
    
    * feat: 유틸 함수 파일 추가
    
    * feat: 타입 관리 파일 추가
    
    * init: liveblocks 캔버스 세팅 및 마이그레이션
    
    * chore: API KEY 없을 경우 에러코드 반환
    
    * docs: PR 템플릿 추가
    
    * docs: PR 템플릿 최종 수정

---
+++ liveblocks.config.ts
@@ -0,0 +1,86 @@
+import {
+  LiveList,
+  LiveMap,
+  LiveObject,
+  createClient,
+} from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+import { Color, Layer, Point } from "@/lib/types";
+
+const client = createClient({
+  authEndpoint: "/api/liveblocks-auth",
+  throttle: 16,
+});
+
+// Presence represents the properties that will exist on every User in the Room
+// and that will automatically be kept in sync. Accessible through the
+// `user.presence` property. Must be JSON-serializable.
+type Presence = {
+  selection: string[];
+  cursor: Point | null;
+  pencilDraft: [x: number, y: number, pressure: number][] | null;
+  penColor: Color | null;
+};
+
+// Optionally, Storage represents the shared document that persists in the
+// Room, even after all Users leave. Fields under Storage typically are
+// LiveList, LiveMap, LiveObject instances, for which updates are
+// automatically persisted and synced to all connected clients.
+type Storage = {
+  layers: LiveMap<string, LiveObject<Layer>>;
+  layerIds: LiveList<string>;
+  person: LiveObject<{
+    name: string;
+  }>;
+  // animals: LiveList<string>,
+  // ...
+};
+
+// Optionally, UserMeta represents static/readonly metadata on each User, as
+// provided by your own custom auth backend (if used). Useful for data that
+// will not change during a session, like a User's name or avatar.
+type UserMeta = {
+  info: {
+    name: string;
+    color: [string, string];
+    avatar: string;
+  };
+};
+
+// Event types, 유니온 타입 가능
+type RoomEvent = {
+  type: "TOAST";
+  message: string;
+};
+
+// Optionally, the type of custom events broadcasted and listened for in this
+// room. Must be JSON-serializable.
+// type RoomEvent = {};
+
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+  },
+} = createRoomContext<Presence, Storage, UserMeta /* RoomEvent */>(client);
