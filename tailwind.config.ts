9857f335e37d6ee19fd63f4eade5ad89739335cf
jjunohj <jjunohj@gmail.com>
2024-03-16 15:41:00 +0900

    style: 커스텀 스타일 설정, src 폴더 스타일 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,6 +13,24 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "surface-canvas": "#eee",
+        "surface-panel": "#fff",
+        "surface-subtle": "#f3f3f3",
+        divider: "#eee",
+        "foreground-600": "#333",
+        "foreground-500": "#444",
+        "foreground-400": "#555",
+        "foreground-300": "#666",
+        "foreground-200": "#888",
+        "foreground-100": "#999",
+        primary: "#0044ff",
+        white: "#fff",
+      },
+      boxShadow: {
+        popup:
+          "0px 2px 4px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.05)",
       },
     },
   },
