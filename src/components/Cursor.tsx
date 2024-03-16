3caa5d76791ae92956215ef6247f07dba45da890
jjunohj <jjunohj@gmail.com>
2024-03-16 18:38:25 +0900

    feat: 이름이 추가된 예쁜 커서 컴포넌트 추가

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,32 +1,60 @@
-import { memo } from "react";
-import { useOther } from "@/liveblocks.config";
-import { connectionIdToColor } from "@/app/lib/utils";
+import { memo, useState, useEffect } from "react";
+import { getContrastingColor } from "@/app/utils/getContrastingColor";
 
 type Props = {
-  connectionId: number;
+  x: number;
+  y: number;
+  color: [string, string];
+  name: string;
 };
 
-function Cursor({ connectionId }: Props) {
-  //
-  // RATIONALE:
-  // Each cursor itself subscribes to _just_ the change for the user. This
-  // means that if only one user's cursor is moving, only one <Cursor />
-  // component has to re-render. All the others can remain idle.
-  //
-  const cursor = useOther(connectionId, (user) => user.presence.cursor);
-  if (!cursor) {
-    return null;
-  }
+function Cursor({ x, y, color = ["", ""], name = "" }: Props) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const textColor = getContrastingColor(color[1]);
 
-  const { x, y } = cursor;
+  useEffect(() => {
+    setPosition({ x, y });
+  }, [x, y]);
+
   return (
-    <path
+    <div
+      className="absolute top-0 left-0 pointer-events-none select-none"
       style={{
-        transform: `translateX(${x}px) translateY(${y}px)`,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        transition: "transform 0.5s cubic-bezier(0.22, 1, 0.36, 1)",
       }}
-      d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
-      fill={connectionIdToColor(connectionId)}
-    />
+    >
+      <div className="relative">
+        <svg
+          className="absolute top-0 left-0"
+          width="32"
+          height="44"
+          viewBox="0 0 24 36"
+          fill="none"
+        >
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="500%" y2="0%">
+              <stop offset="0%" stopColor={color[0]} />
+              <stop offset="100%" stopColor={color[1]} />
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#gradient)"
+            d="M0.928548 2.18278C0.619075 1.37094 1.42087 0.577818 2.2293 0.896107L14.3863 5.68247C15.2271 6.0135 15.2325 7.20148 14.3947 7.54008L9.85984 9.373C9.61167 9.47331 9.41408 9.66891 9.31127 9.91604L7.43907 14.4165C7.09186 15.2511 5.90335 15.2333 5.58136 14.3886L0.928548 2.18278Z"
+          />
+        </svg>
+        <div
+          className="absolute overflow-hidden top-4 left-4 py-1 px-2 font-medium text-sm whitespace-nowrap rounded-lg"
+          style={{
+            backgroundImage: `linear-gradient(to bottom right, ${color[0]}, ${color[1]})`,
+            color: textColor,
+            transition: "background-image 0.5s cubic-bezier(0.22, 1, 0.36, 1)",
+          }}
+        >
+          <div className="z-10 relative">{name}</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
