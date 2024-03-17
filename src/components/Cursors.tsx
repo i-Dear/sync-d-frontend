b1dc92a9d1bdaf07d23cb34ad364235e829e10f6
jjunohj <121740394+jjunohj@users.noreply.github.com>
2024-03-17 13:32:31 +0900

    [#7] Feat: 사용자 이름이 보이는 커서 추가, 커서 좌표 로직 이동 (#8)
    * feat: Cursor 프리센스를 전반적으로 관여하는 Cursors 추가
    
    * feat: 이름이 추가된 예쁜 커서 컴포넌트 추가
    
    * build: 의존성 삭제 - framer-motion
    
    * refactor: Cursor 로직 분리에 따라 Draft도 따로 분리
    
    * refactor: 커서 프리센스 업데이트 논리 Cursors 컴포넌트로 이동

---
+++ src/components/Cursors.tsx
@@ -0,0 +1,88 @@
+import { useOthersMapped, useUpdateMyPresence } from "@/liveblocks.config";
+import Cursor from "./Cursor";
+import { MutableRefObject, useEffect } from "react";
+import { shallow } from "@liveblocks/client";
+import { useBoundingClientRectRef } from "@/app/utils/useBoundingClientRectRef";
+
+type Props = {
+  cursorPanel: MutableRefObject<HTMLElement | null>;
+};
+
+const Cursors = ({ cursorPanel }: Props) => {
+  const updateMyPresence = useUpdateMyPresence();
+
+  const others = useOthersMapped(
+    (other) => ({
+      cursor: other.presence.cursor,
+      info: other.info,
+    }),
+    shallow
+  );
+  const rectRef = useBoundingClientRectRef(cursorPanel);
+
+  useEffect(() => {
+    if (!(cursorPanel?.current instanceof HTMLElement)) {
+      console.warn('"cursorPanel"이 현재 HTMLElement가 아닙니다.');
+      return;
+    }
+
+    const updateCursor = (event: PointerEvent) => {
+      if (!cursorPanel?.current) {
+        return;
+      }
+
+      // (뷰포트 위치) - (이벤트 위치) + (스크롤 위치)
+      const x =
+        event.clientX - rectRef.current.x + cursorPanel.current.scrollLeft;
+      const y =
+        event.clientY - rectRef.current.y + cursorPanel.current.scrollTop;
+
+      updateMyPresence({
+        cursor: {
+          x: Math.round(x),
+          y: Math.round(y),
+        },
+      });
+    };
+
+    const removeCursor = () => {
+      updateMyPresence({
+        cursor: null,
+      });
+    };
+
+    cursorPanel.current.addEventListener("pointermove", updateCursor);
+    cursorPanel.current.addEventListener("pointerleave", removeCursor);
+
+    const oldRef = cursorPanel.current;
+    return () => {
+      if (!oldRef) {
+        return;
+      }
+      oldRef.removeEventListener("pointermove", updateCursor);
+      oldRef.removeEventListener("pointerleave", removeCursor);
+    };
+  }, [updateMyPresence, cursorPanel, rectRef]);
+
+  return (
+    <>
+      {others.map(([id, other]) => {
+        if (other.cursor == null) {
+          return null;
+        }
+
+        return (
+          <Cursor
+            key={id}
+            name={other.info.name}
+            color={other.info.color}
+            x={other.cursor.x}
+            y={other.cursor.y}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Cursors;
