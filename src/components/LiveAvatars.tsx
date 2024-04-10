3ec9a21220c13823c845b1acee1d483b456fff5e
jjunohj <jjunohj@gmail.com>
2024-04-10 18:31:00 +0900

    feat: 캔버스 우측 상단 현재 접속 중인 멤버 표시 컴포넌트 LiveAvatars 구현

---
+++ src/components/LiveAvatars.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Avatar } from "./Avatar";
+import { AnimatePresence, motion } from "framer-motion";
+import { useOthersMapped, useSelf } from "~/liveblocks.config";
+
+const MAX_OTHERS = 3;
+
+const animationProps = {
+  initial: { width: 0, transformOrigin: "left" },
+  animate: { width: "auto", height: "auto" },
+  exit: { width: 0 },
+  transition: {
+    type: "spring",
+    damping: 15,
+    mass: 1,
+    stiffness: 200,
+    restSpeed: 0.01,
+  },
+};
+
+const avatarProps = {
+  style: { marginLeft: "-0.45rem" },
+  size: 40,
+  outlineWidth: 2,
+  outlineColor: "white",
+};
+
+export default function LiveAvatars() {
+  const others = useOthersMapped((other) => other.info);
+  const currentUser = useSelf();
+  const hasMoreUsers = others.length > MAX_OTHERS;
+
+  return (
+    <div
+      style={{
+        minHeight: avatarProps.size + "px",
+        display: "flex",
+        paddingLeft: "0.75rem",
+        overflow: "hidden",
+      }}
+    >
+      <AnimatePresence>
+        {hasMoreUsers ? (
+          <motion.div key="count" {...animationProps}>
+            <Avatar {...avatarProps} variant="more" count={others.length - 3} />
+          </motion.div>
+        ) : null}
+
+        {others
+          .slice(0, MAX_OTHERS)
+          .reverse()
+          .map(([key, info]) => (
+            <motion.div key={key} {...animationProps}>
+              <Avatar
+                {...avatarProps}
+                src={info.avatar}
+                name={info.name}
+                color={info.color}
+              />
+            </motion.div>
+          ))}
+
+        {currentUser ? (
+          <motion.div key="you" {...animationProps}>
+            <Avatar
+              {...avatarProps}
+              src={currentUser.info.avatar}
+              name={currentUser.info.name + " (you)"}
+              color={currentUser.info.color}
+            />
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+    </div>
+  );
+}
