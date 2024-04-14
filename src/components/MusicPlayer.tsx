f40e526f71043ade1320b427fd7f555c61d3b171
jjunohj <121740394+jjunohj@users.noreply.github.com>
2024-04-14 22:42:36 +0900

    Update MusicPlayer.tsx

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -173,137 +173,139 @@ export function MusicPlayer() {
   }, [projectMusic.musicTime]);
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex items-center gap-3">
-        <audio
-          ref={audioRef}
-          src={musics[0].src}
-          preload="true"
-          onLoadedMetadata={setAudioDuration}
-          onTimeUpdate={handleTimeUpdate}
-          onEnded={handleEnded}
-          muted={mute}
-          autoPlay
-        />
-        <div className="w-full flex items-center justify-center gap-4">
-          <div
-            className={cn(
-              "relative bg-neutral-900 w-20 aspect-square rounded-3xl overflow-hidden transition-transform ease-out-back duration-300 shadow-2xl",
-              "after:absolute after:inset-0 after:rounded-[inherit] after:bg-gradient-to-br after:from-transparent after:via-white/5 after:pointer-events-none after:to-transparent",
-              {
-                "scale-95": !playing,
-              }
-            )}
-          >
-            <div className="scale-75">
+    <>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center gap-3">
+          <audio
+            ref={audioRef}
+            src={musics[0].src}
+            preload="true"
+            onLoadedMetadata={setAudioDuration}
+            onTimeUpdate={handleTimeUpdate}
+            onEnded={handleEnded}
+            muted={mute}
+            autoPlay
+          />
+          <div className="w-full flex items-center justify-center gap-4">
+            <div
+              className={cn(
+                "relative bg-neutral-900 w-20 aspect-square rounded-3xl overflow-hidden transition-transform ease-out-back duration-300 shadow-2xl",
+                "after:absolute after:inset-0 after:rounded-[inherit] after:bg-gradient-to-br after:from-transparent after:via-white/5 after:pointer-events-none after:to-transparent",
+                {
+                  "scale-95": !playing,
+                }
+              )}
+            >
+              <div className="scale-75">
+                <Image
+                  className="rounded-full animate-spin-slow select-none"
+                  style={{
+                    animationPlayState: playing ? "running" : "paused",
+                  }}
+                  src={musics[0].cover}
+                  width={384}
+                  height={384}
+                  alt=""
+                />
+              </div>
+              <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <button
+                  type="button"
+                  className="size-4 shadow-xl rounded-full hover:scale-120 transition-transform ease-out-expo duration-500"
+                  onClick={togglePlay}
+                  title={playing ? "Pause" : "Play"}
+                >
+                  <span className="sr-only">{playing ? "Pause" : "Play"}</span>
+                  <PauseIcon
+                    className={cn(
+                      "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-transparent fill-neutral-900 transition duration-300 ease-out-expo",
+                      {
+                        "scale-50 opacity-0": !playing,
+                      }
+                    )}
+                  />
+                  <PlayIcon
+                    className={cn(
+                      "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-transparent fill-neutral-900 ml-0.5 transition duration-300 ease-out-expo",
+                      {
+                        "scale-50 opacity-0": playing,
+                      }
+                    )}
+                  />
+                </button>
+              </span>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <span className="font-medium text-lg">{musics[0].title}</span>
+              <span className="text-neutral-600 text-sm">{musics[0].artist}</span>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-[--wave-height]">
+          {/* Range slider for audio time and waveform */}
+          {duration ? (
+            <Slider.Root
+              className="relative flex-1 flex items-center select-none touch-none w-full h-full"
+              min={0}
+              max={duration}
+              step={1}
+              value={[time]}
+              onValueChange={handleSliderChange}
+              onValueCommit={handleSliderCommit}
+            >
+              <div className="absolute inset-0">
+                <WaveForm percentage={time / duration} src={musics[0].src} />
+              </div>
+              <Slider.Track>
+                <Slider.Range />
+                <Slider.Thumb />
+              </Slider.Track>
+            </Slider.Root>
+          ) : null}
+        </div>
+        <div className="flex">
+          <div>
+            {mute ? (
               <Image
-                className="rounded-full animate-spin-slow select-none"
-                style={{
-                  animationPlayState: playing ? "running" : "paused",
-                }}
-                src={musics[0].cover}
-                width={384}
-                height={384}
-                alt=""
+                src="/mute.svg"
+                alt="Mute"
+                width={24}
+                height={24}
+                onClick={toggleMute}
               />
-            </div>
-            <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-              <button
-                type="button"
-                className="size-4 shadow-xl rounded-full hover:scale-120 transition-transform ease-out-expo duration-500"
-                onClick={togglePlay}
-                title={playing ? "Pause" : "Play"}
-              >
-                <span className="sr-only">{playing ? "Pause" : "Play"}</span>
-                <PauseIcon
-                  className={cn(
-                    "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-transparent fill-neutral-900 transition duration-300 ease-out-expo",
-                    {
-                      "scale-50 opacity-0": !playing,
-                    }
-                  )}
-                />
-                <PlayIcon
-                  className={cn(
-                    "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-transparent fill-neutral-900 ml-0.5 transition duration-300 ease-out-expo",
-                    {
-                      "scale-50 opacity-0": playing,
-                    }
-                  )}
-                />
-              </button>
-            </span>
-          </div>
-          <div className="flex flex-col items-center text-center">
-            <span className="font-medium text-lg">{musics[0].title}</span>
-            <span className="text-neutral-600 text-sm">{musics[0].artist}</span>
+            ) : (
+              <Image
+                src="/sound.svg"
+                alt="Unmute"
+                width={24}
+                height={24}
+                onClick={toggleMute}
+              />
+            )}
           </div>
         </div>
-      </div>
-      <div className="relative h-[--wave-height]">
-        {/* Range slider for audio time and waveform */}
-        {duration ? (
-          <Slider.Root
-            className="relative flex-1 flex items-center select-none touch-none w-full h-full"
-            min={0}
-            max={duration}
-            step={1}
-            value={[time]}
-            onValueChange={handleSliderChange}
-            onValueCommit={handleSliderCommit}
-          >
-            <div className="absolute inset-0">
-              <WaveForm percentage={time / duration} src={musics[0].src} />
-            </div>
-            <Slider.Track>
-              <Slider.Range />
-              <Slider.Thumb />
-            </Slider.Track>
-          </Slider.Root>
-        ) : null}
-      </div>
-      <div className="flex">
         <div>
-          {mute ? (
-            <Image
-              src="/mute.svg"
-              alt="Mute"
-              width={24}
-              height={24}
-              onClick={toggleMute}
-            />
-          ) : (
-            <Image
-              src="/sound.svg"
-              alt="Unmute"
-              width={24}
-              height={24}
-              onClick={toggleMute}
-            />
-          )}
+          <Slider.Root
+            className="relative flex items-center select-none touch-none w-50 h-5"
+            min={0}
+            max={1}
+            step={0.01}
+            value={[volume]}
+            onValueChange={[value]) => {
+              if (!audioRef.current) {
+                return;
+              }
+  
+              setVolume(value);
+              audioRef.current.volume = value;
+            }}
+          >
+            <Slider.Track className="relative bg-neutral-200 flex-grow rounded-full h-1" />
+            <Slider.Range className="absolute bg-white rounded-full h-1" />
+            <Slider.Thumb className="block w-3 h-3 bg-primary rounded-full" />
+          </Slider.Root>
         </div>
       </div>
-      <div>
-        <Slider.Root
-          className="relative flex items-center select-none touch-none w-50 h-5"
-          min={0}
-          max={1}
-          step={0.01}
-          value={[volume]}
-          onValueChange={[value]) => {
-            if (!audioRef.current) {
-              return;
-            }
-
-            setVolume(value);
-            audioRef.current.volume = value;
-          }}
-        >
-          <Slider.Track className="relative bg-neutral-200 flex-grow rounded-full h-1" />
-          <Slider.Range className="absolute bg-white rounded-full h-1" />
-          <Slider.Thumb className="block w-3 h-3 bg-primary rounded-full" />
-        </Slider.Root>
-      </div>
-    </div>
-  );
+    </>
+      );
 }
