821cf2004a56506039d936df376ee24729878a26
jjunohj <jjunohj@gmail.com>
2024-04-14 23:54:57 +0900

    feat: 프로젝트 그룹 콜 입장, 퇴장 로직 및 컴포넌트 추가

---
+++ src/components/GroupCallButton.tsx
@@ -0,0 +1,78 @@
+import { useUserInfoStore } from "@/hooks/useUserInfoStore";
+import SendBirdCall from "sendbird-calls";
+import { useMutation, useStorage } from "~/liveblocks.config";
+import { useSendBirdInit } from "@/hooks/useSendBirdCall";
+
+const GroupCallButton = () => {
+  const userInfo = useUserInfoStore();
+  const groupCallId = useStorage((root) => root.groupCallId);
+
+  const roomParams = {
+    roomType: SendBirdCall.RoomType.SMALL_ROOM_FOR_VIDEO,
+  };
+
+  const enterParams = {
+    videoEnabled: false,
+    audioEnabled: true, // 오디오만 출력
+  };
+
+  // 프로젝트 내 그룹 콜 ID 설정
+  const updateRoomId = useMutation(({ storage }, roomId: string) => {
+    const groupCallId = storage.get("groupCallId");
+    groupCallId.set("roomId", roomId);
+  }, []);
+
+  const authOption = { userId: userInfo._id, accessToken: userInfo.token }; // Authentication options using user information
+  useSendBirdInit(authOption);
+
+  const joinGroupCall = async () => {
+    // 아직 프로젝트 내에 그룹 콜이 진행되지 않은 경우, 새로운 방 생성
+    if (!groupCallId.roomId) {
+      await SendBirdCall.createRoom(roomParams).then((room) => {
+        updateRoomId(room.roomId); // 프로젝트 내 그룹 콜 ID 설정
+        console.log("방 생성 성공, roomId: ", room.roomId);
+      });
+    }
+
+    SendBirdCall.fetchRoomById(groupCallId.roomId).then((room) => {
+      console.log("방 불러오기 성공");
+
+      room
+        .enter(enterParams)
+        .then(() => {
+          console.log("입장 성공: ");
+        })
+        .catch((error) => {
+          console.error("입장 실패: ", error);
+        });
+
+      room.addEventListener("remoteParticipantEntered", (participant) => {
+        console.log("다른 참가자가 입장했습니다. 참가자:", participant);
+      });
+    });
+  };
+
+  const exitGroupCall = () => {
+    const room = SendBirdCall.getCachedRoomById(groupCallId.roomId);
+    room.exit();
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <button
+        className="rounded-xl w-36 h-10 bg-purple-400 text-white"
+        onClick={joinGroupCall}
+      >
+        그룹콜 참여
+      </button>
+      <button
+        className="rounded-xl w-36 h-10 bg-purple-400 text-white"
+        onClick={exitGroupCall}
+      >
+        그룹콜 나가기
+      </button>
+    </div>
+  );
+};
+
+export default GroupCallButton;
