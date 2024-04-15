2420c80b6251d6d719451d237e79e57e961eec5c
jjunohj <jjunohj@gmail.com>
2024-04-16 02:05:05 +0900

    fix: 음성 통화시 목소리가 안들리는 문제 수정

--- src/components/GroupCallButton.tsx
+++ src/components/GroupCallButton.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { useUserInfoStore } from "@/hooks/useUserInfoStore";
 import SendBirdCall from "sendbird-calls";
 import { useMutation } from "~/liveblocks.config";
 import { useSendBirdInit } from "@/hooks/useSendBirdCall";
-import { memo } from "react";
+import { memo, useRef } from "react";
 
 interface TypeGroupCallId {
   roomId: string;
@@ -10,13 +12,14 @@ interface TypeGroupCallId {
 
 const GroupCallButton = memo((groupCallId: TypeGroupCallId) => {
   const userInfo = useUserInfoStore();
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const roomParams = {
-    roomType: SendBirdCall.RoomType.SMALL_ROOM_FOR_VIDEO,
+    roomType: SendBirdCall.RoomType.LARGE_ROOM_FOR_AUDIO_ONLY,
   };
 
   const enterParams = {
-    videoEnabled: true,
+    videoEnabled: false, // 비디오 미사용
     audioEnabled: true, // 오디오만 출력
   };
 
@@ -32,44 +35,61 @@ const GroupCallButton = memo((groupCallId: TypeGroupCallId) => {
   const joinGroupCall = async () => {
     // 아직 프로젝트 내에 그룹 콜이 진행되지 않은 경우, 새로운 방 생성
     if (!groupCallId.roomId) {
-      await SendBirdCall.createRoom(roomParams).then((room) => {
+      SendBirdCall.createRoom(roomParams).then((room) => {
         updateRoomId(room.roomId); // 프로젝트 내 그룹 콜 ID 설정
         console.log("방 생성 성공, roomId: ", room.roomId);
       });
     }
 
     SendBirdCall.fetchRoomById(groupCallId.roomId).then((room) => {
-      console.log("방 불러오기 성공");
+      console.log("방 불러오기 성공: ", room);
 
-      room
-        .enter(enterParams)
-        .then(() => {
-          console.log("입장 성공: ");
-        })
-        .catch((error) => {
-          console.error("입장 실패: ", error);
+      if (
+        room.participants.filter(
+          (participant) => participant.user.userId === userInfo._id
+        ).length > 0
+      ) {
+        console.log("이미 참여한 방입니다.");
+        return;
+      } else {
+        room
+          .enter(enterParams)
+          .then(() => {
+            console.log("입장 성공: ");
+            if (!audioRef.current) return;
+
+            room.setAudioForLargeRoom(audioRef.current); // 오디오 설정
+            audioRef.current!.volume = 0.5; // 볼륨 설정
+            audioRef.current!.muted = false; // 음소거 설정
+          })
+          .catch((error) => {
+            console.error("입장 실패: ", error);
+          });
+
+        room.addEventListener("remoteParticipantEntered", (participant) => {
+          console.log("다른 참가자가 입장했습니다. 참가자:", participant);
         });
 
-      room.addEventListener("remoteParticipantEntered", (participant) => {
-        console.log("다른 참가자가 입장했습니다. 참가자:", participant);
-      });
-
-      room.addEventListener("remoteAudioSettingsChanged", (participant) => {
-        console.log(
-          "참가자의 오디오 설정이 변경되었습니다. 참가자:",
-          participant
-        );
-      });
+        room.addEventListener("remoteAudioSettingsChanged", (participant) => {
+          console.log(
+            "참가자의 오디오 설정이 변경되었습니다. 참가자:",
+            participant
+          );
+        });
+      }
     });
   };
 
   const exitGroupCall = () => {
     const room = SendBirdCall.getCachedRoomById(groupCallId.roomId);
-    room.exit();
+    try {
+      room.exit();
+    } catch (error) {}
   };
 
   return (
     <div className="flex flex-col gap-2">
+      <audio ref={audioRef} autoPlay />
       <button
         className="rounded-xl w-36 h-10 bg-purple-400 text-white"
         onClick={joinGroupCall}
