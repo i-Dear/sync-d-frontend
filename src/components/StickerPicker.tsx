3f780a7f9824adf5e8da7186c3d37f691bbee903
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-04-16 17:18:01 +0900

    Feat: 스티커 버튼 클릭시 스티커 셀렉터 렌더 기능 구현

---
+++ src/components/StickerPicker.tsx
@@ -0,0 +1,16 @@
+import stickerData from "@/lib/sticker.json";
+import Image from "next/image";
+
+const StickerPicker = () => {
+  return (
+    <div className="absolute bottom-1/2 transform -translate-x-1/2 -translate-y-1/4 bg-white p-4 rounded-lg shadow-md z-10 flex flex-col items-center justify-center max-w-600">
+      <div className="flex gap-4  flex-wrap justify-center">
+        {stickerData.map((sticker, i) => (
+          <Image className=" hover:bg-gray-200  cursor-pointer" src={sticker.src} alt={""} key={i} width={48} height={48} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StickerPicker;
