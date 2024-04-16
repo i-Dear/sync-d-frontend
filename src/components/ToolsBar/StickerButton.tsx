04cfdb235a729ddaaadd9d4fa5bfc61f338a50b2
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-04-16 17:18:01 +0900

    Feat: 스티커 버튼 클릭시 스티커 셀렉터 렌더 기능 구현

--- src/components/ToolsBar/StickerButton.tsx
+++ src/components/ToolsBar/StickerButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import IconButton from "../IconButton";
+import StickerPicker from "../StickerPicker";
 
 type Props = {
   isActive: boolean;
@@ -8,24 +9,27 @@ type Props = {
 
 export default function StickerButton({ isActive, onClick }: Props) {
   return (
-    <IconButton isActive={isActive} onClick={onClick}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round">
-        <path d="M22 11v1a10 10 0 1 1-9-10" />
-        <path d="M8 14s1.5 2 4 2 4-2 4-2" />
-        <line x1="9" x2="9.01" y1="9" y2="9" />
-        <line x1="15" x2="15.01" y1="9" y2="9" />
-        <path d="M16 5h6" />
-        <path d="M19 2v6" />
-      </svg>
-    </IconButton>
+    <>
+      <IconButton isActive={isActive} onClick={onClick}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round">
+          <path d="M22 11v1a10 10 0 1 1-9-10" />
+          <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+          <line x1="9" x2="9.01" y1="9" y2="9" />
+          <line x1="15" x2="15.01" y1="9" y2="9" />
+          <path d="M16 5h6" />
+          <path d="M19 2v6" />
+        </svg>
+      </IconButton>
+      <div>{isActive && <StickerPicker />}</div>
+    </>
   );
 }
