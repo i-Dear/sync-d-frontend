83f81251d93d1cb07d212094390ac4b4cb4bccd5
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-04-17 14:10:13 +0900

    Feat: 스티커 선택창에서 선택 시 onSelect로 툴바(캔버스)까지 전달

--- src/components/ToolsBar/StickerButton.tsx
+++ src/components/ToolsBar/StickerButton.tsx
@@ -5,9 +5,10 @@ import StickerPicker from "../StickerPicker";
 type Props = {
   isActive: boolean;
   onClick: () => void;
+  onSelectSticker: (stickerSrc: string) => void;
 };
 
-export default function StickerButton({ isActive, onClick }: Props) {
+export default function StickerButton({ isActive, onClick, onSelectSticker }: Props) {
   return (
     <>
       <IconButton isActive={isActive} onClick={onClick}>
@@ -18,9 +19,9 @@ export default function StickerButton({ isActive, onClick }: Props) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
           <path d="M22 11v1a10 10 0 1 1-9-10" />
           <path d="M8 14s1.5 2 4 2 4-2 4-2" />
           <line x1="9" x2="9.01" y1="9" y2="9" />
@@ -29,7 +30,15 @@ export default function StickerButton({ isActive, onClick }: Props) {
           <path d="M19 2v6" />
         </svg>
       </IconButton>
-      <div>{isActive && <StickerPicker />}</div>
+      <div>
+        {isActive && (
+          <StickerPicker
+            onSelectSticker={(stickerSrc) => {
+              onSelectSticker(stickerSrc);
+            }}
+          />
+        )}
+      </div>
     </>
   );
 }
