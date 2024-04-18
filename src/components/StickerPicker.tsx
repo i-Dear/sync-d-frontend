81d443a4298b42c1b509486d227da5b09e7f0cfa
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-04-18 15:03:54 +0900

    Refactor: ContextAPI Zustand로 변경

--- src/components/StickerPicker.tsx
+++ src/components/StickerPicker.tsx
@@ -1,12 +1,12 @@
 // StickerPicker.js
 
 import React, { useState, useContext } from "react";
-import { StickerContext } from "./context/StickerContext";
 import stickerData from "@/lib/sticker.json";
 import Image from "next/image";
+import useStickerStore from "@/store/useStickerSrcStore";
 
 const StickerPicker = () => {
-  const { setSticker } = useContext(StickerContext);
+  const { setStickerSrc } = useStickerStore();
 
   return (
     <div className="absolute bottom-1/2 transform -translate-x-1/2 -translate-y-1/4 bg-white p-4 rounded-lg shadow-md z-10 flex flex-col items-center justify-center max-w-600">
@@ -19,7 +19,7 @@ const StickerPicker = () => {
             key={i}
             width={48}
             height={48}
-            onClick={() => setSticker(sticker.src)}
+            onClick={() => setStickerSrc(sticker.src)}
           />
         ))}
       </div>
