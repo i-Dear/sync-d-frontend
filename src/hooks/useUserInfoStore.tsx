71a57b2b982dcc985a79714a5dc7a28c2064a3d6
jjunohj <jjunohj@gmail.com>
2024-04-18 20:49:58 +0900

    chore: UserInfo에 전역속성 color 삭제

--- src/hooks/useUserInfoStore.tsx
+++ src/hooks/useUserInfoStore.tsx
@@ -3,16 +3,14 @@ import { create } from "zustand";
 export type UserInfoStoreType = {
   _id: string;
   name: string;
-  color: string;
   hostingRooms: string[];
   joinedRooms: string[];
   token: string;
 };
 
-export const useUserInfoStore = create<UserInfoStoreType>((set) => ({
+const useUserInfoStore = create<UserInfoStoreType>((set) => ({
   _id: "",
   name: "",
-  color: "",
   hostingRooms: [],
   joinedRooms: [],
   token: "",
