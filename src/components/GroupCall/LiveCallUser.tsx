03d60124f364cbbb0d9ebbcca77a6e17ff391d37
jjunohj <jjunohj@gmail.com>
2024-04-21 19:51:34 +0900

    feat: 음성통화 진입 시간에 따른 실시간 접속 시간 디스플레이 구현

--- src/components/GroupCall/LiveCallUser.tsx
+++ src/components/GroupCall/LiveCallUser.tsx
@@ -2,32 +2,43 @@
 
 import { formatDuration } from "@/lib/utils";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { ActiveUserInfo } from "~/liveblocks.config";
 
 const LiveCallUser = ({ user }: { user: ActiveUserInfo }) => {
+  const [time, setTime] = useState(
+    user.enteredAt ? user.enteredAt : Date.now(),
+  );
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="flex h-fit w-fit items-center justify-between border-b border-gray-200 px-4 py-2">
-      <div className="flex items-center space-x-2">
-        <div className="relative h-8 w-8">
-          {user.avatar && (
+    <div className="flex h-[40px] w-[281px] items-center justify-between border-gray-200">
+      <div className="flex items-center">
+        <div className="relative h-[40px] w-[40px] rounded-full bg-gray-50 shadow-sm">
+          {user.avatar ? (
             <Image
               src={user.avatar}
               alt={user.name}
               layout="fill"
               objectFit="cover"
-              className="rounded-full"
             />
-          )}
-          {!user.avatar && (
-            <div className="h-full w-full rounded-full bg-gray-200">
+          ) : (
+            <div className="flex h-full w-full items-center justify-center font-semibold">
               {user.name[0]}
             </div>
           )}
         </div>
-        <div className="flex flex-col items-start justify-center gap-2">
-          <span className="font-semibold">{user.name}</span>
-          <span className="text-xs text-gray-500">
-            {formatDuration(user.enteredAt)}
+        <div className="ml-[16px] flex flex-col items-start justify-center">
+          <span className="text-lg font-semibold text-div-text">
+            {user.name}
+          </span>
+          <span className="text-base font-normal text-time">
+            {formatDuration(time)}
           </span>
         </div>
       </div>
