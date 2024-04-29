967a1fe689313e70c0b825014f5879a876bbc7b9
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-04-29 17:28:13 +0900

    Feat: 모달 스토어 및 컴포넌트 구현

---
+++ src/components/Modal.tsx
@@ -0,0 +1,15 @@
+interface ModalProps {
+  onClose: () => void;
+}
+
+const Modal = ({ onClose }: ModalProps) => {
+  return (
+    <div className="fixed left-0 top-0 z-30 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70 text-center">
+      <div className="w-10/12 bg-white md:w-1/3" onClick={() => onClose()}>
+        <div className="p-[8px]">asdasddsa</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
