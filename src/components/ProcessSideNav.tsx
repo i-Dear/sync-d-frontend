26a87e4f6aade049f521774cd00ad0365b1e4336
jjunohj <jjunohj@gmail.com>
2024-05-01 15:31:54 +0900

    feat: 정적 프로세스에서 라이브 블록 프로세스로 변경, UI 및 애니메이션 구현

--- src/components/ProcessSideNav.tsx
+++ src/components/ProcessSideNav.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import Link from "next/link";
-import { steps } from "@/lib/data";
 import type { UserInfoStoreType } from "@/hooks/useUserInfoStore";
-import { SetStateAction, useEffect } from "react";
+import { SetStateAction, useState } from "react";
 import { Camera } from "@/lib/types";
-import { useUpdateMyPresence } from "~/liveblocks.config";
+import {
+  useMutation,
+  useStorage,
+  useUpdateMyPresence,
+} from "~/liveblocks.config";
 import ProcessAvatars from "./ProcessAvatars";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const ProcessSideNav = ({
   userInfo,
@@ -16,41 +21,96 @@ const ProcessSideNav = ({
   setCamera: React.Dispatch<SetStateAction<Camera>>;
 }) => {
   const updateMyPresence = useUpdateMyPresence();
+  const processes = useStorage((storage) => storage.process);
+  const [currentProcess, setCurrentProcess] = useState(1);
+  const [isToggle, setIsToggle] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggle((prev) => !prev);
+  };
+
+  // 프로세스 완료 시 진행
+  // const updateProcessCompleted = useMutation(({ storage }, step: number) => {
+  //   const processes = storage.get("process");
+
+  //   if (!processes.get(step - 1)) {
+  //     console.log("Invalid step");
+  //     return;
+  //   }
+
+  //   if (processes.get(step - 1)!.done) {
+  //     return;
+  //   }
+
+  //   processes.get(step - 1)!.done = true;
+  // }, []);
 
   const updateCurrentProcess = (step: number) => {
     updateMyPresence({
       currentProcess: step,
     });
+    setCurrentProcess(step);
   };
 
   return (
-    <nav className="absolute left-0 z-10 h-full w-40 overflow-y-scroll bg-gray-900 scrollbar-hide">
-      <ul className="flex flex-col items-start justify-center ">
-        <li className="p-4 text-white">
-          <Link href="/">홈</Link>
-        </li>
-        <li className="p-4 text-white">
-          <Link href={`/dashboard/${userInfo._id}`}>대시보드</Link>
-        </li>
-        {steps.map((step) => (
-          <li key={step.step} className="p-4 text-white">
-            <div
-              className="flex cursor-pointer"
+    <motion.nav
+      className="absolute left-0 z-10 flex h-full flex-col items-start justify-start bg-white pt-[1rem] shadow-md shadow-light-gray-100"
+      initial={{ width: "7.2rem" }}
+      animate={{ width: isToggle ? "34.6rem" : "7.2rem" }}
+    >
+      <div
+        className="mb-[1.2rem] ml-[1.6rem] flex h-[4rem] w-[4rem] cursor-pointer items-center justify-center p-[1rem]"
+        onClick={handleToggle}
+      >
+        <Image src="/menu.svg" alt="Menu" width={20} height={18} />
+      </div>
+      <div className="absolute left-[9rem] top-[0.85rem] flex h-[4rem] w-[30rem] items-center justify-start gap-[1.6rem]">
+        <span className="h-full text-[1.8rem] font-semibold leading-[4.3rem] tracking-[-0.03rem]">
+          {`${currentProcess}. ${processes[currentProcess - 1].title}`}
+        </span>
+        <Image src="/help.svg" alt="Help" width={18} height={18} />
+      </div>
+
+      <ul className="flex flex-col overflow-y-scroll scrollbar-hide">
+        {processes.map((process) => (
+          <li key={process.step}>
+            <motion.div
+              className="flex h-[6.4rem] cursor-pointer items-center justify-start px-[1.6rem] py-[1.2rem]"
+              initial={{ width: "7.2rem" }}
+              animate={{ width: isToggle ? "34.6rem" : "7.2rem" }}
+              transition={{ duration: 0.3 }}
               onClick={() => {
                 setCamera(() => ({
-                  x: step.camera.x,
-                  y: step.camera.y,
+                  x: process.camera.x,
+                  y: process.camera.y,
                 }));
-                updateCurrentProcess(step.step);
+                updateCurrentProcess(process.step);
               }}
             >
-              {`${step.step}단계`}
-              <ProcessAvatars step={step.step} />
-            </div>
+              <div
+                className={cn(
+                  "flex h-[4rem] w-[4rem] cursor-pointer items-center justify-center rounded-[1rem] bg-light-gray-100",
+                  {
+                    "bg-primary-300": process.step === currentProcess,
+                  },
+                )}
+              >
+                <span className="h-[4rem] w-[4rem] text-center text-[1.8rem] font-normal leading-[4.4rem] text-div-text">
+                  {process.step}
+                </span>
+              </div>
+
+              <div className="ml-[1.6rem] flex items-center justify-start gap-[1.2rem] overflow-hidden">
+                <span className="h-[2rem] text-[1.6rem] font-light leading-[2.4rem] text-div-text">
+                  {process.title}
+                </span>
+                <ProcessAvatars step={process.step} />
+              </div>
+            </motion.div>
           </li>
         ))}
       </ul>
-    </nav>
+    </motion.nav>
   );
 };
 
