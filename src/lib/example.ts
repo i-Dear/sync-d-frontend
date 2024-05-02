d22f0dcbd0c6f715619a567888d2ca2f123fe625
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-05-02 23:13:51 +0900

    Chore: 가이드 예시 이미지 추가

---
+++ src/lib/example.ts
@@ -0,0 +1,45 @@
+export const exampleImages = [
+  [
+    {
+      name: "step1-1",
+      src: "/steps/step1.png",
+    },
+    {
+      name: "step1-2",
+      src: "/steps/step2.png",
+    },
+    {
+      name: "step1-3",
+      src: "/steps/step3.png",
+    },
+  ],
+  [
+    {
+      name: "step1",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/100px-Noto_Emoji_KitKat_263a.svg.png",
+    },
+    {
+      name: "grin",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Emoji_u1f600.svg/100px-Emoji_u1f600.svg.png",
+    },
+    {
+      name: "squinting",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Emoji_u1f61d.svg/100px-Emoji_u1f61d.svg.png",
+    },
+  ],
+
+  [
+    {
+      name: "step1",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/100px-Noto_Emoji_KitKat_263a.svg.png",
+    },
+    {
+      name: "grin",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Emoji_u1f600.svg/100px-Emoji_u1f600.svg.png",
+    },
+    {
+      name: "squinting",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Emoji_u1f61d.svg/100px-Emoji_u1f61d.svg.png",
+    },
+  ],
+];
