2ba68040fe70d0266a9c97131270326f95a87769
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-03 02:03:06 +0900

    Chore: 가이드 src 수정

--- src/lib/guideImages.ts
+++ src/lib/guideImages.ts
@@ -2,43 +2,43 @@ export const exampleImages = [
   [
     {
       name: "step1-1",
-      src: "/steps/step1.png",
+      src: "/steps/stepguide1.png",
     },
     {
       name: "step1-2",
-      src: "/steps/step2.png",
+      src: "/steps/stepguide2.png",
     },
     {
       name: "step1-3",
-      src: "/steps/step3.png",
+      src: "/steps/stepguide3.png",
     },
   ],
   [
     {
       name: "step2-1",
-      src: "/steps/step3.png",
+      src: "/steps/stepguide3.png",
     },
     {
       name: "step2-2",
-      src: "/steps/step1.png",
+      src: "/steps/stepguide1.png",
     },
     {
       name: "step2-3",
-      src: "/steps/step2.png",
+      src: "/steps/stepguide2.png",
     },
   ],
   [
     {
       name: "step3-1",
-      src: "/steps/step2.png",
+      src: "/steps/stepguide2.png",
     },
     {
       name: "step3-2",
-      src: "/steps/step1.png",
+      src: "/steps/stepguide1.png",
     },
     {
       name: "step3-3",
-      src: "/steps/step3.png",
+      src: "/steps/stepguide3.png",
     },
   ],
 ];
