cf7007485f266377608a21bc25fbe73e6ba982c1
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-03 16:38:02 +0900

    Refactor: 모달 스토어 내 currentProcess삭제

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -2,13 +2,11 @@ import { create } from "zustand";
 
 interface ModalStoreType {
   isOpen: boolean;
-  currentProcess: number;
   changeModalState: () => void;
 }
 
 const useModalStore = create<ModalStoreType>((set) => ({
   isOpen: false,
-  currentProcess: 1,
   changeModalState: () => set((state) => ({ isOpen: !state.isOpen })),
 }));
 
