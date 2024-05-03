5eaad6daa3e8633e381c55b35cdd81055b423f75
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-03 21:00:59 +0900

    Style: 스킵모달 스타일링

--- src/components/Modals/SkipModal.tsx
+++ src/components/Modals/SkipModal.tsx
@@ -14,15 +14,15 @@ const SkipModal = () => {
   }, []);
 
   return (
-    <div className="fixed left-0 top-0 z-30 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70 px-[24rem] py-[4rem] text-center">
-      <div className=" flex h-full w-full flex-col bg-white">
+    <div className="fixed left-0 top-0 z-30 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70 px-[50rem] py-[40rem] text-center">
+      <div className=" space-around flex h-full w-full flex-col bg-white">
         <div className="border-grey-100 flex h-[50px] items-center justify-center border p-[8px]">
           스킵
         </div>
-        <div className="full">{currentProcess}단계를 건너뛸까요?</div>
+        <div className="h-full">{currentProcess}단계를 건너뛸까요?</div>
         <div className="border-grey-100 flex h-[50px] items-center justify-center border p-[8px]">
           <button
-            className="bg-grey w-[80px] cursor-pointer rounded-2xl  p-2 text-center text-[18px] text-white"
+            className="w-[80px] cursor-pointer rounded-2xl bg-gray-700  p-2 text-center text-[18px] text-white"
             onClick={() => setModalState(false)}
           >
             닫기
