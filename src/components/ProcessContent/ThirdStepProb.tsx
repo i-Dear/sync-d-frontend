a3c66d9186ca826e9aaef7a20b476dce1209fcd3
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-05 18:05:09 +0900

    Feat: 3단계 문제상황 템플릿 구현

---
+++ src/components/ProcessContent/ThirdStepProb.tsx
@@ -0,0 +1,33 @@
+import { ThirdStepProbTemplate } from "@/lib/types";
+
+export default function ThirdStepProb(props: ThirdStepProbTemplate) {
+  const { title, x, y, width, height, fill } = props;
+
+  return (
+    <g>
+      <ellipse
+        style={{
+          transform: `translate(${x}px, ${y}px)`,
+        }}
+        cx={width / 2}
+        cy={height / 2}
+        rx={width / 2}
+        ry={height / 2}
+        fill="#FFF"
+        stroke="#F0F2F5"
+        strokeWidth="4"
+      />
+      {props.title && (
+        <text
+          x={x + 95}
+          y={y + 220}
+          fontFamily="Arial"
+          fontSize="200"
+          fill="#F0F2F5"
+        >
+          {title}
+        </text>
+      )}
+    </g>
+  );
+}
