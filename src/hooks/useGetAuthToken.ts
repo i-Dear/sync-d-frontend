4139eae2f1369ef47056078b635388b347614f41
jjunohj <jjunohj@gmail.com>
2024-05-06 19:25:57 +0900

    feat: authToken 불러오는 커스텀 훅 구현

---
+++ src/hooks/useGetAuthToken.ts
@@ -0,0 +1,26 @@
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const useGetAuthToken = () => {
+  const [token, setToken] = useState<string | null>(null);
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const localToken = localStorage.getItem("authToken");
+      if (localToken) {
+        setToken(localToken);
+      } else {
+        const authToken = searchParams.get("authToken");
+        if (authToken) {
+          localStorage.setItem("authToken", authToken);
+          setToken(authToken);
+        }
+      }
+    }
+  }, [searchParams]);
+
+  return token;
+};
+
+export default useGetAuthToken;
