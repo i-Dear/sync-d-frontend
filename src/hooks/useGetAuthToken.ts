d3931ea4b4c65a442f4a9c843f5255c0517751ae
jjunohj <jjunohj@gmail.com>
2024-05-10 15:50:52 +0900

    feat: 로그인 후 URL에 액세스 토큰 정보 삭제

--- src/hooks/useGetAuthToken.ts
+++ src/hooks/useGetAuthToken.ts
@@ -1,18 +1,22 @@
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const useGetAuthToken = () => {
   const [token, setToken] = useState<string | null>(null);
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     const localToken = localStorage.getItem("authToken");
     if (localToken) {
       setToken(localToken);
+      router.replace(pathname);
     } else {
       const authToken = searchParams.get("authToken");
       if (authToken) {
         localStorage.setItem("authToken", authToken);
+        router.replace(pathname);
         setToken(authToken);
       }
     }
