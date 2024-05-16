60a1b5e83e00b8f843ad983b107f3abe5b649026
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-17 02:15:00 +0900

    Chore: 유저스토리 시나리오 fetch 원상복구 및 주석 삭제

--- src/utils/processSync.ts
+++ src/utils/processSync.ts
@@ -56,50 +56,6 @@ export const fetchScenario = async (
   }
 };
 
-// export const addEpicTemplate = (
-//   templates: LiveList<Template>,
-//   epic: Epic,
-//   idx: number,
-// ) => {
-//   templates.push({
-//     id: `${parseInt(epic.id) + 1100}`,
-//     type: TemplateType.EpicBox,
-//     length: epic.userStories.length,
-//     title: epic.name,
-//     x:
-//       idx < 4
-//         ? 50 + (parseInt(epic.id) - 1) * 400
-//         : 50 + (parseInt(epic.id) - 5) * 400,
-//     y: idx < 4 ? 10100 : 10600,
-//     fill: "#369EFF",
-//     width: 380,
-//   });
-// };
-
-// export const addUserStoryTemplate = (
-//   templates: LiveList<Template>,
-//   epic: Epic,
-//   epidx: number,
-//   userStory: UserStory,
-//   idx: number,
-// ) => {
-//   templates.push({
-//     id: `${parseInt(epic.id) * 100 + 1100 + userStory.id}`,
-//     type: TemplateType.EpicBox,
-//     length: 1,
-//     title: userStory.name,
-//     x:
-//       epidx < 4
-//         ? 50 + (parseInt(epic.id) - 1) * 400
-//         : 50 + (parseInt(epic.id) - 5) * 400,
-//     y: epidx < 4 ? 10100 + userStory.id * 60 : 10600 + userStory.id * 60,
-//     width: 380,
-//     height: 50,
-//     fill: "#A5D0F9",
-//     font: 8,
-//   });
-// };
-
 export const addEpicLayer = (
   layers: LiveMap<string, LiveObject<Layer>>,
   epic: Epic,
