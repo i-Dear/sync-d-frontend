e02a92f12cdb20d4462cbb63041813b15f2c84c4
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-17 02:13:30 +0900

    Feat: Epic 내 유저스토리 삭제 추가

--- src/components/CanvasLayer/Epic.tsx
+++ src/components/CanvasLayer/Epic.tsx
@@ -3,6 +3,7 @@ import { useMutation } from "~/liveblocks.config";
 import { UserStory, LayerType } from "@/lib/types";
 import { useStorage } from "~/liveblocks.config";
 import PlusMarkIcon from "~/public/PlusMark";
+import XMarkIcon from "~/public/Xmark";
 import { useState } from "react";
 import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 import { EpicLayer } from "@/lib/types";
@@ -20,7 +21,7 @@ export default function Epic({
   id,
   selectionColor,
 }: EpicProps) {
-  const { x, y, width, length, title, value } = layer;
+  const { x, y, width, length, height, title, value } = layer;
 
   const handleChangeTitle = (key: string, value: string) => {
     if (key === "title") {
@@ -55,14 +56,35 @@ export default function Epic({
     [],
   );
 
-  console.log("value", value);
+  const handleAdd = useMutation(({ storage }) => {
+    const liveLayers = storage.get("layers");
+    const prevStory = liveLayers.get(id)?.get("value") as UserStory[];
+    prevStory.push({ id: prevStory.length, name: "" });
+    const newStory = prevStory;
+    liveLayers.get(id)?.set("value", newStory);
+    const newHeight = newStory.length * 70 + 160;
+    liveLayers.get(id)?.set("height", newHeight);
+  }, []);
+
+  const handleDeleteValue = (index: number) => {
+    updateDelete(index);
+  };
+
+  const updateDelete = useMutation(({ storage }, index: number) => {
+    const liveLayers = storage.get("layers");
+    const prevStory = liveLayers.get(id)?.get("value") as UserStory[];
+    const newStory = prevStory.filter((_, i) => i !== index);
+    liveLayers.get(id)?.set("value", newStory);
+    const newHeight = newStory.length * 70 + 160;
+    liveLayers.get(id)?.set("height", newHeight);
+  }, []);
 
   return (
     <foreignObject
       x={x}
       y={y}
       width={width}
-      height={length * 100 + 100}
+      height={height === 0 ? length * 70 + 160 : height}
       style={{ background: "#E9F5FF" }}
       onPointerDown={(e) => onPointerDown(e, id)}
       className=" shadow-grey-950  rounded-lg p-[2rem] shadow-lg drop-shadow-lg"
@@ -81,19 +103,29 @@ export default function Epic({
       <div className="my-[2rem] flex flex-col gap-[2rem]">
         {value &&
           value.map((item, index) => (
-            <ContentEditable
-              key={item.id}
-              html={item.name || " "}
-              onChange={(e) => handleChangeValue(index, e.target.value)}
-              className="flex h-[5rem] w-full items-center justify-center rounded-lg bg-primary-200 p-[1rem] text-3xl font-medium outline-none "
-              style={{
-                fontSize: 14,
-                color: "black",
-              }}
-            />
+            <div className="relative" key={item.id}>
+              <ContentEditable
+                html={item.name || " "}
+                onChange={(e) => handleChangeValue(index, e.target.value)}
+                className="flex h-[5rem] w-full items-center justify-center rounded-lg bg-primary-200 p-[1rem] text-3xl font-medium outline-none "
+                style={{
+                  fontSize: 14,
+                  color: "black",
+                }}
+              />
+              <div
+                className="absolute right-[-2.2rem] top-[1.5rem] cursor-pointer rounded-full p-1"
+                onClick={() => handleDeleteValue(index)}
+              >
+                <XMarkIcon fill="grey" width={15} height={15} />
+              </div>
+            </div>
           ))}
       </div>
-      <div className="flex h-[5rem] w-full items-center justify-center rounded-lg bg-primary-200 p-[1rem] text-3xl font-medium outline-none">
+      <div
+        onClick={handleAdd}
+        className="flex h-[5rem] w-full items-center justify-center rounded-lg bg-primary-200 p-[1rem] text-3xl font-medium outline-none"
+      >
         <PlusMarkIcon fill="black" />
       </div>
     </foreignObject>
