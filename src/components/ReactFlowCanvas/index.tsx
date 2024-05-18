e7cd3fff311ab209b989c9b4236022b41f699057
jjunohj <jjunohj@gmail.com>
2024-05-18 20:41:14 +0900

    feat: 모든 노드, 엣지 초기화하는 init 함수 추가, 뷰포트 설정

--- src/components/ReactFlowCanvas/index.tsx
+++ src/components/ReactFlowCanvas/index.tsx
@@ -30,6 +30,8 @@ import InputForWhat from "./InputForWhat";
 import initialNodes from "@/lib/nodes";
 import MiddleNode from "./MiddleNode";
 import AnnotationNode from "./AnnotationNode";
+import PageNode from "./PageNode";
+import ContentNode from "./ContentNode";
 
 type Viewport = { x: number; y: number; zoom: number };
 
@@ -37,8 +39,8 @@ const StepViewport: Record<number, Viewport> = {
   5: { x: 0, y: 0, zoom: 1 },
   6: { x: -600, y: 0, zoom: 1 },
   7: { x: -1050, y: 0, zoom: 1 },
-  9: { x: 0, y: 1000, zoom: 1 },
-  12: { x: 0, y: 2000, zoom: 1 },
+  9: { x: 0, y: -1000, zoom: 1 },
+  12: { x: 0, y: -1500, zoom: 0.8 },
 };
 
 const connectionLineStyle = {
@@ -50,6 +52,8 @@ const nodeTypes = {
   stakeholderNode: StakeholderNode,
   middleNode: MiddleNode,
   annotation: AnnotationNode,
+  pageNode: PageNode,
+  contentNode: ContentNode,
 };
 
 const edgeTypes = {
@@ -215,18 +219,19 @@ const Flow = ({ currentProcess }: { currentProcess: number }) => {
     [edges],
   );
 
-  const initNodes = useMutation(({ storage }) => {
+  const init = useMutation(({ storage }) => {
     storage.set("nodes", initialNodes);
+    storage.set("edges", []);
   }, []);
 
   useEffect(() => {
     if (nodes.length === 0) {
-      initNodes();
+      init();
     }
-  }, [nodes, initNodes]);
+  }, []);
 
-  // 노드 초기화 버튼 (주석 풀면 초기화)
-  //initNodes();
+  // 노드엣지 초기화 버튼 (주석 풀면 초기화)
+  //init();
 
   return (
     <div className="h-full w-full grow" ref={reactFlowWrapper}>
