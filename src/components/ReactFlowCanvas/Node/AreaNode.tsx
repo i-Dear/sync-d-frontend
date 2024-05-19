2a3135ed017b65b4707c4cc4cd450bacccc3d1f1
jjunohj <jjunohj@gmail.com>
2024-05-20 01:51:53 +0900

    feat: 그룹 노드 AreaNode 구현

---
+++ src/components/ReactFlowCanvas/Node/AreaNode.tsx
@@ -0,0 +1,22 @@
+import { memo } from "react";
+import { Node } from "reactflow";
+
+const AreaNode = ({ data }: Pick<Node, "data">) => {
+  return (
+    <>
+      <span className="absolute left-[1.4rem] top-[1.4rem] gap-[0.8rem] text-[1.6rem] font-semibold text-black">
+        {data.label}
+        {data.description.map((description: string, index: number) => (
+          <span
+            key={index}
+            className="block text-[1.2rem] font-normal text-black"
+          >
+            {description}
+          </span>
+        ))}
+      </span>
+    </>
+  );
+};
+
+export default memo(AreaNode);
