c0daeb454689c13c2bbed4c3eb0a1b4f8e35be02
jjunohj <jjunohj@gmail.com>
2024-05-24 19:06:35 +0900

    style: 싱크 모달 리퍼블리시

--- src/components/Modals/SyncModal.tsx
+++ src/components/Modals/SyncModal.tsx
@@ -7,6 +7,8 @@ import useModalStore from "@/store/useModalStore";
 import { Process } from "@/lib/types";
 import { SetStateAction, useState } from "react";
 import { Camera } from "@/lib/types";
+import Lottie from "react-lottie";
+import checkJson from "~/public/lotties/check.json";
 
 interface ModalProps {
   setCamera: React.Dispatch<SetStateAction<Camera>>;
@@ -35,20 +37,30 @@ const SyncedModal = ({ setCamera }: ModalProps) => {
     });
   };
 
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: checkJson,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className=" space-around flex h-[20rem] w-[60rem] flex-col bg-white">
-      <div className="border-grey-100 flex h-[50px] items-center justify-center border p-[8px]">
-        Synced!
+    <div className="flex h-[fit] w-[40rem] flex-col items-center justify-start gap-[0.8rem] rounded-[2rem] bg-white px-[4.45rem] py-[3.2rem] shadow-2xl">
+      <div className="flex h-[16rem] w-[16rem] items-center justify-center">
+        <Lottie options={defaultOptions} height={200} width={200} />
       </div>
-      <div className="h-full">{latestUndoneStep - 1}단계가 완료되었습니다!</div>
-      <div className="border-grey-100 flex h-[50px] items-center justify-center gap-[4rem] border p-[8px]">
-        <button
-          className="w-[80px] cursor-pointer rounded-2xl bg-gray-700  p-2 text-center text-[18px] text-white"
-          onClick={handleClick}
-        >
-          닫기
-        </button>
-      </div>
+      <span className="text-[3.6rem] font-bold text-div-text">Good Job !</span>
+      <span className="text-[1.6rem] font-normal text-div-text">
+        {latestUndoneStep - 1}단계를 완료했습니다!
+      </span>
+      <button
+        className="w-fit cursor-pointer rounded-[1.2rem] bg-gray-700 px-[1.2rem] py-[0.8rem] text-center text-[1.4rem] font-normal text-white"
+        onClick={handleClick}
+      >
+        다음 단계로 이동
+      </button>
     </div>
   );
 };
