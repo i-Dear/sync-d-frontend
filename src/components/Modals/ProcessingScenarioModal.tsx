bf6ef33ce3721b4d3e61c015e6ead73b8eb93112
jjunohj <jjunohj@gmail.com>
2024-05-24 19:06:19 +0900

    style: 유저 시나리오 작성 대시 모달 리퍼블리시

--- src/components/Modals/ProcessingScenarioModal.tsx
+++ src/components/Modals/ProcessingScenarioModal.tsx
@@ -1,9 +1,27 @@
+import Lottie from "react-lottie";
+import writingJson from "~/public/lotties/writing.json";
+
 const ProcessingScenarioModal = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: writingJson,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className=" space-around flex h-[20rem] w-[60rem] flex-col bg-white">
-      <div className="h-full">
-        시나리오를 바탕으로 유저 스토리를 생성중입니다.. 잠시 기다려주세요..
+    <div className="flex h-[fit] w-[40rem] flex-col items-center justify-start gap-[0.8rem] rounded-[2rem] bg-white px-[4.45rem] py-[3.2rem] shadow-2xl">
+      <div className="flex h-[22rem] w-[22rem] items-center justify-center">
+        <Lottie options={defaultOptions} height={240} width={240} />
       </div>
+      <span className="text-[2.8rem] font-bold text-div-text">
+        잠시만 기다려주세요!
+      </span>
+      <span className="text-[1.6rem] font-normal text-div-text">
+        Sync-D가 작성해주신 시나리오를 바탕으로 유저 스토리를 작성하고 있어요.
+      </span>
     </div>
   );
 };
